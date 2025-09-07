0bbca5d819b3248d692d5b33a08d3a3987495741
him40706 <wmeili174@gmail.com>
2025-09-07 10:55:31 +0800

    儀錶板優化

--- v1.11.7/resources/scripts/components/elements/StatusIndicator.tsx
+++ v1.11.7/resources/scripts/components/elements/StatusIndicator.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
+import { ServerPowerState } from '../../api/server/getServerResourceUsage';
 import tw from 'twin.macro';
 import styled from 'styled-components/macro';
-import { ServerPowerState } from '../../api/server/getServerResourceUsage';
 
 type ServerStatus = ServerPowerState | 'installing' | 'suspended' | 'restoring_backup' | 'transferring';
 
-const StatusIndicatorBox = styled.div<{ $status: ServerStatus | undefined }>`
+const StatusIndicatorBox = styled.div<{ $status: ServerStatus | null | undefined }>`
     ${tw`w-3 h-3 rounded-full bg-red-500 transition-colors duration-150`};
 
     ${({ $status }) =>
@@ -17,4 +18,6 @@ const StatusIndicatorBox = styled.div<{ $status: ServerStatus | undefined }>`
             : tw`bg-yellow-500`};
 `;
 
-export default StatusIndicatorBox;
+export default ({ status }: { status: ServerStatus | null | undefined }) => {
+    return <StatusIndicatorBox $status={status} />;
+};
