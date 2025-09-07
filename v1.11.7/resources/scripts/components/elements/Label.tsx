8632dd528c057b9dc3758413f1f8d6bf74a4e6b3
him40706 <wmeili174@gmail.com>
2025-09-07 14:08:15 +0800

    顯示優化

--- v1.11.7/resources/scripts/components/elements/Label.tsx
+++ v1.11.7/resources/scripts/components/elements/Label.tsx
@@ -1,12 +1,8 @@
+import styled from 'styled-components/macro';
 import tw from 'twin.macro';
 
-const Label = tw.label`
-    block
-    text-sm
-    font-semibold
-    text-neutral-700
-    dark:text-neutral-200
-    mb-2
+const Label = styled.label`
+    ${tw`block text-sm font-semibold text-neutral-700 dark:text-neutral-200 mb-2`}
 `;
 
 export default Label;
