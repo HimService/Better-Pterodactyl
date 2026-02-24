138d41d5428410d276e8bd8f69277eee2fe43f0c
him40706 <wmeili174@gmail.com>
2026-02-24 20:12:14 +0800

    up date v1.12.1

--- v1.12.1/resources/views/admin/nodes/view/settings.blade.php
+++ v1.12.1/resources/views/admin/nodes/view/settings.blade.php
@@ -80,7 +80,7 @@
                         <div>
                             <div class="radio radio-success radio-inline">
                                 <input type="radio" id="pSSLTrue" value="https" name="scheme" {{ (old('scheme', $node->scheme) === 'https') ? 'checked' : '' }}>
-                                <label for="pSSLTrue"> @lang('admin/node.new.ssl_help')</label>
+                                <label for="pSSLTrue"> @lang('admin/node.new.ssl_help_ssl')</label>
                             </div>
                             <div class="radio radio-danger radio-inline">
                                 <input type="radio" id="pSSLFalse" value="http" name="scheme" {{ (old('scheme', $node->scheme) !== 'https') ? 'checked' : '' }}>
