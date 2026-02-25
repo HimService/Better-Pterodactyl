725723f41e21f146a6c801d2c5f9c7d969837c03
him40706 <wmeili174@gmail.com>
2026-02-25 21:37:20 +0800

    up date i18 system

--- v1.12.1/resources/scripts/i18n.ts
+++ v1.12.1/resources/scripts/i18n.ts
@@ -4,19 +4,18 @@ import { initReactI18next } from 'react-i18next';
 import zh_TW from '../locales/zh.json';
 import en from '../locales/en.json';
 import ja from '../locales/ja.json';
+import config from './config';
 
 i18n.use(initReactI18next).init({
     debug: process.env.DEBUG === 'true',
-    lng: (window as any).PterodactylUser?.language || 'zh',
-    fallbackLng: 'en',
+    lng: (window as any).PterodactylUser?.language || config.login_language || (window as any).SiteLocale || (typeof navigator !== 'undefined' && navigator.language) || config.fallback_language,
+    fallbackLng: config.fallback_language,
     keySeparator: '.',
     ns: ['translation', 'frontend'],
     defaultNS: 'translation',
     resources: {
         zh_TW: { translation: zh_TW, frontend: zh_TW },
-        'zh-TW': { translation: zh_TW, frontend: zh_TW },
         zh: { translation: zh_TW, frontend: zh_TW },
-        'en': { translation: en, frontend: en },
         en: { translation: en, frontend: en },
         ja: { translation: ja, frontend: ja },
     },
